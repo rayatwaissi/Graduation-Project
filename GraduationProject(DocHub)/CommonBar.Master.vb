a4bf4d3f290bdcae574f72e7b0ca42ac373344f5
yasmeen444 <yasmenk644@gmail.com>
2026-03-26 00:47:19 +0300

    add missing fies

---
+++ GraduationProject(DocHub)/CommonBar.Master.vb
@@ -0,0 +1,11 @@
+﻿Public Class CommonBar
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub lnkUpload_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
